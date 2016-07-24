e8aaab5bd63620d4c127e5a8309d3234759ae44c
zhangyantai <zhangyantai@github.com>
2016-07-24 14:41:28 +0800

    PhoneLaout update

--- app/src/main/java/com/example/lsx/phonelayout/MainActivity.java
+++ app/src/main/java/com/example/lsx/phonelayout/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.lsx.phonelayout;
 
-import android.Manifest;
+
+
 import android.content.Intent;
-import android.content.pm.PackageManager;
+
 import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -11,33 +12,24 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
     EditText number = null;
-
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         number = (EditText) findViewById(R.id.edit_txt);
-
     }
 
     public void dial(View v) {
         String result = number.getText().toString().trim();
-        if (result.equals("") && result == null) {
+        if (result.equals("") || result == null) {
             Toast.makeText(this, "电话号码不能为空", Toast.LENGTH_LONG).show();
             return;
         }
-        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + result));
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + result));
         startActivity(intent);
     }
 
